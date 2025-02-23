e123eddecb56fbc76d4151ee38b6594f04a14481
ZhengjunHUO <firelouiszj@hotmail.com>
2025-02-23 09:59:20 +0100

    explore with bpftrace suite
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

--- bpftrace/cmd.sh
+++ bpftrace/cmd.sh
@@ -20,3 +20,21 @@ sudo bpftrace -l "tracepoint:syscalls:sys_exit*" | wc -l
 
 # sudo bpftrace -e 'kprobe:vfs_read { @start[tid] = nsecs; } kretprobe:vfs_read /@start[tid]/ { @ns[comm] = hist(nsecs - @start[tid]); delete(@start, tid); }'
 # sudo bpftrace -e 'kprobe:vfs_read { @start[tid] = nsecs; } kretprobe:vfs_read /@start[tid]/ { @ns[comm] = hist(nsecs - @start[tid]); delete(@start[tid]); }'
+
+# Count process-level events for five seconds, quit and print a summary.
+sudo bpftrace -e 'tracepoint:sched:sched* { @[probe] = count(); } interval:s:5 { exit(); }'
+#Attaching 29 probes...
+#@[tracepoint:sched:sched_migrate_task]: 85
+#@[tracepoint:sched:sched_wake_idle_without_ipi]: 3319
+#@[tracepoint:sched:sched_wakeup]: 3913
+#@[tracepoint:sched:sched_waking]: 3939
+#@[tracepoint:sched:sched_stat_runtime]: 6927
+#@[tracepoint:sched:sched_switch]: 7026
+
+# Profile kernel stacks at 99 Hertz, printing a frequency count
+# sudo bpftrace -e 'profile:hz:99 { @[kstack] = count(); }'
+# counts stack traces that led to context switching (off-CPU) events
+# sudo bpftrace -e 'tracepoint:sched:sched_switch { @[kstack] = count(); }'
+
+# Block I/O requests by size in bytes
+# sudo bpftrace -e 'tracepoint:block:block_rq_issue { @ = hist(args.bytes); }'
